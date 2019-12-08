fb4d142decb62a4524db920978528f17eabd58c5
Екатерина Хлюпина <murzikheh@gmail.com>
2019-12-08 21:30:51 +0300

    3

--- CourseProject/DocumentService.cs
+++ CourseProject/DocumentService.cs
@@ -14,40 +14,17 @@ namespace CourseProjekt
 {
     public class DocumentService
     {
-        public static string PArseWordX()
+        public static string PArseWordX(Stream file)
         {
             //using (var doc = WordprocessingDocument.Open(@"C:\Users\Пушистая булка\Desktop\Учеба\Result_v5.docx", false))
-            using (var doc = WordprocessingDocument.Open(@"C:\Users\Admin\Desktop\Учеба\Result_v5.docx", false))
+            //using (var doc = WordprocessingDocument.Open(@"C:\Users\Admin\Desktop\Учеба\Result_v5.docx", false))
+            using (var doc = WordprocessingDocument.Open(file, false))
             {
                 var body = doc.MainDocumentPart.Document.Body.InnerText;
                 return body;
             }
         }
 
-        public static string DefaultToUTF(string s)
-        {
-            byte[] bytes = Encoding.Default.GetBytes(s);
-            s = Encoding.UTF8.GetString(bytes);
-            return s;
-        }
-
-
-        public static string UTF8ToWin1251(string sourceStr) // Конвертирует строку из UTF-8 в Windows-1251
-        {
-            Encoding utf8 = Encoding.UTF8;
-            Encoding win1251 = Encoding.GetEncoding("Windows - 1251");
-            byte[] utf8Bytes = utf8.GetBytes(sourceStr);
-            byte[] win1251Bytes = Encoding.Convert(utf8, win1251, utf8Bytes);
-            return win1251.GetString(win1251Bytes);
-        }
-        public static string Win1251ToUTF8(string source)//Конвертирует строку из Windows-1251 в UTF-8
-        {
-            Encoding utf8 = Encoding.GetEncoding("utf - 8");
-            Encoding win1251 = Encoding.GetEncoding("windows - 1251");
-            byte[] utf8Bytes = win1251.GetBytes(source);
-            byte[] win1251Bytes = Encoding.Convert(win1251, utf8, utf8Bytes);
-            source = win1251.GetString(win1251Bytes);
-            return source;
-        }
+       
     }
 }
--- CourseProject/Pages/Index.cshtml.cs
+++ CourseProject/Pages/Index.cshtml.cs
@@ -1,29 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Web;
+using Microsoft.AspNetCore.Http;
+using CourseProjekt;
 
 namespace CourseProject.Pages
 {
     public class IndexModel : PageModel
     {
-        public string Message { get; set; }
+        public string Text { get; set; }
+        public static string Key { get; set; }
+        public string Message2 { get; set; }
+        public bool Locker = false;
 
-        public void OnGet()
+        public static string ErrorMessage { get; set; }
+
+        public static string TempDocumentText { get; set; }
+
+        public void OnPostUpload(IFormFile uploadfile)
         {
-            Message = "Введите текст для расшифроки";
+            if (uploadfile != null)
+            {
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    uploadfile.CopyTo(ms);
+                    TempDocumentText = DocumentService.PArseWordX(ms);
+                }
+                Locker = true;
+                Text = TempDocumentText;
+            }
 
         }
-        public void OnPost(string text, string key)
+
+        public void OnPostText(string text, string key)
         {
-            var cipher = new VigenereCipher(text, key);
-
-            Message = cipher.Decrypt();
-
+            try
+            {
+                if (text == null) { text = ""; }
+                else
+                {
+                    ErrorMessage = null;
+                    if (TempDocumentText != null)
+                    {
+                        text = TempDocumentText;
+                        Text = TempDocumentText;
+                        TempDocumentText = null;
+                    }
+                    var cipher = new VigenereCipher(text, key);
+                    Message2 = cipher.Decrypt();
+                    Text = text;
+                    Key = key;
+                }
+            }
+            catch (Exception)
+            {
+                ErrorMessage = "Уважаемый, вы не ввели ключ. Не надо так.";
+            }
         }
-
-
     }
 }
